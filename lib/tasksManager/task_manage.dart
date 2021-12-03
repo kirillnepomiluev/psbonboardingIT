9bcef45e7c1d78fd9b0409ad3b626b614421e3f4
Илья <morozovilya-coder163>
2021-12-03 15:15:46 +0300

    fix task_manage (add datePicker)

--- lib/tasksManager/task_manage.dart
+++ lib/tasksManager/task_manage.dart
@@ -7,16 +7,21 @@ import 'package:flutter_app_digital_finals/home/widgets/box_task.dart';
 import 'package:flutter_app_digital_finals/main.dart';
 import 'package:flutter_app_digital_finals/themes/colors.dart';
 import 'package:flutter_app_digital_finals/widgets/buttons.dart';
+import 'package:intl/intl.dart';
 
 class TaskManageWidget extends StatefulWidget {
   final String employeeID;
-  TaskManageWidget({Key? key, required this.employeeID}) : super (key: key);
+
+  TaskManageWidget({Key? key, required this.employeeID}) : super(key: key);
+
   @override
   State<TaskManageWidget> createState() => _TaskManageWidgetState();
 }
 
 class _TaskManageWidgetState extends State<TaskManageWidget> {
+  var _birthDate = DateTime.now();
   TextEditingController controllerTaskName = TextEditingController();
+  TextEditingController controllerHeader = TextEditingController();
 
   bool taskOnline = false;
 
@@ -24,107 +29,252 @@ class _TaskManageWidgetState extends State<TaskManageWidget> {
   Widget build(BuildContext context) {
     if (FirebaseAuth.instance.currentUser == null) {
       return Container();
-    }
-    else {
-      return  Column(
+    } else {
+      return Column(
         children: [
           tasksList(widget.employeeID),
           Container(
-          child: Column(children: [
-               Text("Новая задача"),
-               TextFormField(
-                 decoration: InputDecoration( labelText: "описание"),
-                 controller: controllerTaskName,
-
-               ),
-              Container( ), // TODO тут нужен выбор времени
-            Row(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: const Color(0xFFF9F9F9),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-               const Text("Задача online" ),
-                CupertinoSwitch(
-                  activeColor: blueTextPSB,
-                  onChanged: (value){
-                    taskOnline = value;
-                  },
-                  value: taskOnline,
+                const Text("Новая задача",style: TextStyle(color: blackTextPSB,fontWeight: FontWeight.w400,fontSize: 20,fontFamily: 'Gilroy'),),
+                Container(height: 8,),
+                _textField(
+                  labelText: "Заголовок",
+                  controller: controllerHeader,
                 ),
+                Container(height: 8,),
+                _textField(
+                  labelText: "Описание",
+                  controller: controllerTaskName,
+                ),
+                Container(height: 8,),
+                CustomDateSelector(
+                  context,
+                  const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                  "Birth Date",
+                  _birthDate,
+                  _onBirthDateChanged,
+                  const BoxDecoration(
+                    color: Color(0xFFF9F9F9)
+                  ),
+                ),
+                Container(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("Задача online",style: TextStyle(fontFamily: 'Gilroy',fontSize: 16,color: blackTextPSB,fontWeight: FontWeight.w400),),
+                    CupertinoSwitch(
+                      activeColor: blueTextPSB,
+                      onChanged: (value) {
+                        setState(() {
+                          taskOnline = value;
+                        });
+                      },
+                      value: taskOnline,
+                    ),
+                  ],
+                ),
+                Container(height: 10,),
+                bigButton(
+                    onPressed: () {
+                      store
+                          .collection("users")
+                          .doc(widget.employeeID)
+                          .collection("tasks")
+                          .add({
+                        'online': taskOnline,
+                        "textTask": controllerTaskName.text,
+                        "headerTextTask": controllerHeader.text,
+                        "completed": false,
+                        "time": _birthDate
+                      });
+                    },
+                    text: "Добавить")
               ],
             ),
-            bigButton(onPressed: () {
-               store.collection("users").doc(widget.employeeID).collection("tasks").add({
-                 'online' :  taskOnline ,
-                 "textTask" : controllerTaskName.text,
-                 "completed" : false,
-                 "time": DateTime.now()
-               });
-            } , text: "Добавить")
-
-          ],),
           )
         ],
       );
     }
   }
 
- Widget  tasksList(String userID) {
-   if (FirebaseAuth.instance.currentUser == null) {
-     return Container();
-   }
-   else {
-     return Container(
-       margin: const EdgeInsets.only(bottom: 15),
-       child: Column(
-         children: [
-           appTitle(title: 'Задачи на сегодня'),
-           StreamBuilder(
-             stream: store
-                 .collection("users")
-                 .doc(userID)
-                 .collection("tasks")
-                 .snapshots(),
-             builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-               if (snapshot.hasData) {
-                 QuerySnapshot snapData = snapshot.data;
-                 return ListView.builder(
-                   physics: const NeverScrollableScrollPhysics(),
-                   shrinkWrap: true,
-                   itemCount: snapData.docs.length,
-                   itemBuilder: (context, item) {
-                     Map<String, dynamic> data =
-                     snapData.docs[item].data() as Map<String, dynamic>;
+  Widget _textField({TextEditingController? controller,String labelText = 'Описание'}){
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(68)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFieldFilter(
+                context,
+                const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        topRight: Radius.circular(8),
+                        bottomLeft: Radius.circular(8),
+                        bottomRight: Radius.circular(8))),
+                hinttext: labelText,
+                controller: controller,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-                     int maxItem = snapData.docs.length;
+  void _onBirthDateChanged(DateTime value) {
+    setState(() {
+      _birthDate = value;
+    });
+  }
 
-                     final bool lastItem = item == maxItem - 1;
-                     final bool firstItem = item == 0;
-                     bool nextCompleted = false;
-                     if (!lastItem) {
-                       nextCompleted = (snapData.docs[item + 1].data() as Map<
-                           String,
-                           dynamic>)["completed"];
-                     }
 
-                     return BoxTask(
-                       firstItem: firstItem,
-                       time: (data['time'] as Timestamp).toDate(),
-                       online: data["online"],
-                       completed: data["completed"],
-                       textTask: data["textTask"].toString(),
-                       listItem: lastItem,
-                       docSt: snapData.docs[item],
-                       nextCompleted: nextCompleted,
-                     );
-                   },
-                 );
-               } else {
-                 return CircularProgressIndicator();
-               }
-             },
-           ),
-         ],
-       ),
-     );
-   }
+  Widget tasksList(String userID) {
+    if (FirebaseAuth.instance.currentUser == null) {
+      return Container();
+    } else {
+      return Container(
+        margin: const EdgeInsets.only(bottom: 15),
+        child: Column(
+          children: [
+            appTitle(title: 'Задачи на сегодня'),
+            StreamBuilder(
+              stream: store
+                  .collection("users")
+                  .doc(userID)
+                  .collection("tasks")
+                  .snapshots(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  QuerySnapshot snapData = snapshot.data;
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: snapData.docs.length,
+                    itemBuilder: (context, item) {
+                      Map<String, dynamic> data =
+                          snapData.docs[item].data() as Map<String, dynamic>;
 
- }
+                      int maxItem = snapData.docs.length;
+
+                      final bool lastItem = item == maxItem - 1;
+                      final bool firstItem = item == 0;
+                      bool nextCompleted = false;
+                      if (!lastItem) {
+                        nextCompleted = (snapData.docs[item + 1].data()
+                            as Map<String, dynamic>)["completed"];
+                      }
+
+                      return BoxTask(
+                        firstItem: firstItem,
+                        time: (data['time'] as Timestamp).toDate(),
+                        online: data["online"],
+                        completed: data["completed"],
+                        textTask: data["textTask"].toString(),
+                        listItem: lastItem,
+                        docSt: snapData.docs[item],
+                        nextCompleted: nextCompleted,
+                      );
+                    },
+                  );
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  Widget CustomDateSelector(
+      BuildContext context,
+      EdgeInsetsGeometry margin,
+      String hint,
+      DateTime dateTime,
+      Function(DateTime) callback,
+      BoxDecoration decoration) {
+    return Container(
+      margin: margin,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: decoration,
+      child: InkWell(
+        onTap: () {
+          final firstDate = DateTime(DateTime.now().year - 100);
+          final lastDate = DateTime(DateTime.now().year - 18, DateTime.now().month, DateTime.now().day);
+
+          showDatePicker(
+            context: context,
+            initialDate: dateTime,
+            firstDate: firstDate.isBefore(dateTime) ? firstDate : dateTime,
+            lastDate: lastDate.isAfter(dateTime) ? lastDate : dateTime,
+            errorFormatText: '',
+            errorInvalidText: '',
+          ).then((value) => {if (value != null) callback(value)});
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              hint,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 14,
+                  color: lightBlackTextPSB),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 4.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Text(
+                      dateFullFromMilliseconds(dateTime.millisecondsSinceEpoch),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          fontFamily: 'Roboto',
+                          color: blackTextPSB),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.keyboard_arrow_down_rounded,
+                    color: blueTextPSB,
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String dateFullFromMilliseconds(int time) {
+    if (time == null) {
+      return "Not assigned";
+    }
+    DateTime date = DateTime.fromMillisecondsSinceEpoch((time));
+
+    return DateFormat('dd MMMM yyyy', 'en_US').format(date);
+  }
+
+
 }
