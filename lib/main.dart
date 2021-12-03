7ab2765fe0edda951d83476c5ebe8372dfadab88
adnreych <adnreych@gmail.com>
2021-12-03 16:34:43 +0300

    убирает дебаг-ленту

--- lib/main.dart
+++ lib/main.dart
@@ -88,6 +88,7 @@ class _MyAppState extends State<MyApp> {
       //данные настроек
     ],
     child: MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: initialRout,
       onGenerateRoute: router.Router.generateRoute,
       title: 'LupMe',
